39a405a88386084e602b653b99512b0be341fd56
Chibuzor Okwusiuno <cokwusiu@uoguelph.ca>
2024-12-29 21:28:31 -0500

    worked on error messages

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,8 +9,11 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodPost, "/jobs", app.createNewJobHandler)
 	router.HandlerFunc(http.MethodGet, "/jobs/:id", app.showJobHandler)
 
-	return router
+	return app.recoverPanic(router)
 }
